02ce8b9302bc4dc0940138d269f266c801655a51
duc anh <ducanh.nl2@gmail.com>
2023-03-14 15:58:53 +0700

    first nuxt

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="app-layout">
+    <Nuxt />
+    <p>Made by dz</p>
+  </div>
+</template>
+<script>
+export default {}
+</script>
+<style lang="scss">
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  background-color: rgb(144, 226, 176);
+  .app-layout {
+    p {
+      text-align: center;
+    }
+  }
+}
+</style>
---
+++ store/todo.js
@@ -0,0 +1,77 @@
+export const state = () => ({
+  todoList: []
+})
+
+export const getters = {
+  all(state) {
+    return state.todoList
+  },
+  uncomplete(state) {
+    return state.todoList.filter((item) => !item.done)
+  },
+  completed(state) {
+    return state.todoList.filter((item) => item.done)
+  }
+}
+
+export const mutations = {
+  store(state, data) {
+    state.todoList = data
+  },
+  add(state, content) {
+    state.todoList.push(content)
+  },
+  update(state, todo) {
+    const index = state.todoList.findIndex((item) => item.id === todo.id)
+    state.todoList[index].done = todo.done
+  },
+  del(state, todo) {
+    const index = state.todoList.findIndex((item) => item.id === todo.id)
+    state.todoList.splice(index, 1)
+  }
+}
+
+export const actions = {
+  getTodoList(vuexContext) {
+    return this.$axios
+      .$get('https://640fee7ee1212d9cc9250d67.mockapi.io/api/v1/todo')
+      .then((res) => {
+        vuexContext.commit('store', res)
+      })
+  },
+  postTodolist(vuexContext, data) {
+    return this.$axios
+      .$post('https://640fee7ee1212d9cc9250d67.mockapi.io/api/v1/todo', data)
+      .then((res) => {
+        vuexContext.commit('add', res)
+      })
+      .catch((err) => {
+        alert(err)
+      })
+  },
+  updateTodolist(vuexContext, todo) {
+    return this.$axios
+      .$put(
+        `https://640fee7ee1212d9cc9250d67.mockapi.io/api/v1/todo/${todo.id}`,
+        {
+          done: !todo.done
+        }
+      )
+      .then((res) => {
+        vuexContext.commit('update', res)
+      })
+      .catch((err) => {
+        alert(err)
+      })
+  },
+  delTodolist(vuexContext, id) {
+    return this.$axios
+      .$delete(`https://640fee7ee1212d9cc9250d67.mockapi.io/api/v1/todo/${id}`)
+      .then((res) => {
+        vuexContext.commit('del', res)
+      })
+      .catch((err) => {
+        alert(err)
+      })
+  }
+}
