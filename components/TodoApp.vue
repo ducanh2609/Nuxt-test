18da43227ec0601051319496591b294ae5d002f2
duc anh <ducanh.nl2@gmail.com>
2023-03-15 18:05:22 +0700

    final commit

--- components/TodoApp.vue
+++ components/TodoApp.vue
@@ -1,35 +1,19 @@
 <template>
   <div class="todoapp-box">
     <h1>Todo List</h1>
-    <input
-      v-model="newValue"
-      class="todo-new-input"
-      type="text"
-      placeholder="New todo"
-      @keypress.enter="post()"
-    />
+    <input v-model="newValue" class="todo-new-input" type="text" placeholder="New todo" @keypress.enter=" status === '' ? post() : update()" />
     <div class="todo-list-box">
-      <div
-        v-for="(todo, index) in filterTodos"
-        :key="todo.id"
-        class="todo-list-item"
-        :class="todo.done ? 'complete' : ''"
-      >
-        <span @click="updateTodolist(todo)"
-          >{{ index + 1 }} . {{ todo.todo }}</span
-        >
-        <i class="fa-solid fa-trash" @click="delTodolist(todo.id)"></i>
+      <div v-for="todo in filterTodos" :key="todo.id">
+        <TodoItems :todo="todo"></TodoItems>
       </div>
     </div>
-    <div class="todo-button">
-      <div class="todo-filter">
-        <input v-model="filter" type="radio" value="all" checked /> All <br />
-        <input v-model="filter" type="radio" value="completed" /> Complete
-        <br />
-        <input v-model="filter" type="radio" value="uncomplete" /> Not Complete
+    <div class="todo-filter">
+      <div><input v-model="filter" type="radio" value="all" checked /> All</div>
+      <div>
+        <input v-model="filter" type="radio" value="completed" /> Completed
       </div>
-      <div class="todo-delete-all">
-        <button class="todo-delete-button">Delete All</button>
+      <div>
+        <input v-model="filter" type="radio" value="uncomplete" /> Not Complete
       </div>
     </div>
   </div>
@@ -42,7 +26,7 @@ export default {
       newValue: '',
       value: '',
       filter: 'all',
-      todoList: []
+      status: ''
     }
   },
   computed: {
@@ -54,20 +38,32 @@ export default {
     this.getTodoList()
   },
   methods: {
-    ...mapActions('todo', [
-      'getTodoList',
-      'postTodolist',
-      'delTodolist',
-      'updateTodolist'
-    ]),
+    ...mapActions('todo', ['getTodoList']),
     post() {
+      let id
+      if (this.filterTodos[this.filterTodos.length - 1]) {
+        id = this.filterTodos[this.filterTodos.length - 1].id + 1
+      } else {
+        id = 1
+      }
       const data = {
-        id: this.filterTodos[this.filterTodos.length - 1].id + 1,
+        id,
         todo: this.newValue,
         done: false
       }
       this.postTodolist(data)
       this.newValue = ''
+    },
+    update() {
+      const data = {
+        status: this.status,
+        id: this.idEdit,
+        todo: this.newValue,
+      }
+      this.updateTodolist(data)
+      this.newValue = ''
+      this.idEdit = -1
+      this.status = ''
     }
   }
 }
@@ -76,16 +72,18 @@ export default {
 .todoapp-box {
   background-color: rgb(235, 205, 150);
   width: 35%;
-  height: 90vh;
+  height: 85vh;
   margin: 30px auto;
   border-radius: 10px;
   box-shadow: 5px 5px 20px olive;
   padding: 10px 40px 30px;
   box-sizing: border-box;
+
   h1 {
     text-align: center;
     font-size: 40px;
   }
+
   .todo-new-input {
     height: 50px;
     width: 97%;
@@ -95,14 +93,17 @@ export default {
     font-size: 20px;
     padding: 0 10px;
     margin-bottom: 20px;
+
     &::placeholder {
       color: rgba(221, 99, 99, 0.479);
     }
+
     &:focus {
       scale: 1.05;
       box-shadow: 3px 3px 5px black;
     }
   }
+
   .todo-list-box {
     height: 70%;
     background-color: white;
@@ -112,68 +113,23 @@ export default {
     box-sizing: border-box;
     font-size: 30px;
     overflow: auto;
+
     &::-webkit-scrollbar {
       width: 2px;
       background-color: rgb(235, 205, 150);
     }
+
     &::-webkit-scrollbar-thumb {
       background-color: orange;
     }
-    .todo-list-item {
-      background-color: rgb(231, 206, 95);
-      height: 50px;
-      line-height: 50px;
-      border-radius: 5px;
-      padding: 0 10px;
-      margin-bottom: 10px;
-      display: flex;
-      justify-content: space-between;
-      &:hover {
-        cursor: pointer;
-        opacity: 0.8;
-      }
-      span {
-        width: 90%;
-      }
-      i {
-        color: rgba(255, 0, 0, 0.288);
-        position: relative;
-        top: 10px;
-        &:hover {
-          cursor: pointer;
-          color: rgb(255, 0, 0);
-        }
-      }
-    }
-    .complete {
-      background-color: rgb(98, 214, 98);
-      span {
-        text-decoration: line-through;
-      }
-    }
   }
-  .todo-button {
+
+  .todo-filter {
+    font-size: 20px;
     display: flex;
     justify-content: space-between;
     margin-top: 20px;
-    .todo-filter {
-      font-size: 20px;
-    }
-    .todo-delete-all {
-      .todo-delete-button {
-        height: 40px;
-        color: white;
-        background-color: rgba(207, 69, 69, 0.767);
-        border: none;
-        border-radius: 3px;
-        padding: 10px;
-        font-size: 20px;
-        &:hover {
-          cursor: pointer;
-          background-color: red;
-        }
-      }
-    }
   }
+
 }
 </style>
--- store/todo.js
+++ store/todo.js
@@ -23,6 +23,7 @@ export const mutations = {
   },
   update(state, todo) {
     const index = state.todoList.findIndex((item) => item.id === todo.id)
+    state.todoList[index].todo = todo.todo
     state.todoList[index].done = todo.done
   },
   del(state, todo) {
@@ -49,20 +50,33 @@ export const actions = {
         alert(err)
       })
   },
-  updateTodolist(vuexContext, todo) {
-    return this.$axios
-      .$put(
-        `https://640fee7ee1212d9cc9250d67.mockapi.io/api/v1/todo/${todo.id}`,
-        {
-          done: !todo.done
-        }
-      )
-      .then((res) => {
-        vuexContext.commit('update', res)
-      })
-      .catch((err) => {
-        alert(err)
-      })
+  updateTodolist(vuexContext, data) {
+    if (!data.status)
+      return this.$axios
+        .$put(
+          `https://640fee7ee1212d9cc9250d67.mockapi.io/api/v1/todo/${data.id}`,
+          {
+            done: !data.done
+          }
+        )
+        .then((res) => {
+          vuexContext.commit('update', res)
+        })
+        .catch((err) => {
+          alert(err)
+        })
+    else
+      return this.$axios
+        .$put(
+          `https://640fee7ee1212d9cc9250d67.mockapi.io/api/v1/todo/${data.id}`,
+          { todo: data.todo }
+        )
+        .then((res) => {
+          vuexContext.commit('update', res)
+        })
+        .catch((err) => {
+          alert(err)
+        })
   },
   delTodolist(vuexContext, id) {
     return this.$axios
